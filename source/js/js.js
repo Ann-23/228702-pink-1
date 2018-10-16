dd05b6f17bbaaaaf5b60f90056ee500bc65e0c33
Анна <anuta.gradna@yandex.ru>
2018-10-16 23:07:08 +0300

    Fixed menu

--- source/js/js.js
+++ source/js/js.js
@@ -1,14 +1,19 @@
+var pageHeader = document.querySelector(".page-header");
 var navMain = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".main-nav__toggle");
 
 navMain.classList.remove("main-nav--nojs");
 
 navToggle.addEventListener("click", function() {
-    if (navMain.classList.contains("main-nav--closed")) {
+    if (navMain.classList.contains("main-nav--closed") & pageHeader.classList.contains("page-header--closed")) {
       navMain.classList.remove("main-nav--closed");
       navMain.classList.add("main-nav--opened");
+      pageHeader.classList.remove("page-header--closed");
+      pageHeader.classList.add("page-header--opened");
     } else {
       navMain.classList.add("main-nav--closed");
       navMain.classList.remove("main-nav--opened");
+      pageHeader.classList.add("page-header--closed");
+      pageHeader.classList.remove("page-header--opened");
     }
 });
