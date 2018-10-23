7459534c98546b3343776f573a320db82a5942a0
Анна <anuta.gradna@yandex.ru>
2018-10-24 00:37:32 +0300

    Made pop-ups

--- source/js/popup.js
+++ source/js/popup.js
@@ -1,19 +1,20 @@
 var popupNotDone = document.querySelector(".pop-up--not-done");
-var linkOk = document.querySelector(".button-ok");
 var popupDone = document.querySelector(".pop-up--done");
-var linkClose = document.querySelector(".button-close");
+var linkOk = document.querySelector(".button-ok");
+var link = document.querySelector(".button-close");
 var form = document.querySelector("form");
 var surname = document.querySelector("[name=surname]");
 var name = document.querySelector("[name=name]");
 var email = document.querySelector("[name=email]");
 
+console.log("surname.value", surname.value);
+console.log("name.value", name.value);
+console.log("email.value", email.value);
+
 form.addEventListener("submit", function (evt) {
   if (!surname.value || !name.value || !email.value) {
     evt.preventDefault();
     popupNotDone.classList.add("pop-up--show");
-    surname.classList.add("input__control--red");
-    name.classList.add("input__control--red");
-    email.classList.add("input__control--red");
 
     linkOk.addEventListener("click", function (evt) {
       evt.preventDefault();
@@ -23,7 +24,7 @@ form.addEventListener("submit", function (evt) {
     evt.preventDefault();
     popupDone.classList.add("pop-up--show");
 
-    linkClose.addEventListener("click", function (evt) {
+    link.addEventListener("click", function (evt) {
       evt.preventDefault();
       popupDone.remove("pop-up--show");
     });
